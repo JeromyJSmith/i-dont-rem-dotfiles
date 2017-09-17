5819d539db414b3f95af0a02284c7193e232a4fe
Kevin Quinn <kpquinn2@wisc.edu>
2017-09-17 14:53:55 -0500

    Finish basic install script for files

--- install_script.sh
+++ install_script.sh
@@ -1,6 +1,34 @@
 #! /bin/bash
+. ~/dotfiles/functions.sh
 
 
 # Can have section for root directory easy ones, then any in weird places we can specifically symlink?
 
+dir=~/dotfiles
+olddir=~/old_dotfiles
+ignorefiles="README.md install_script.sh boilerplate_funcs"
 
+
+echo "Create backup folder for any existing dotfiles.."
+mkdir -p $olddir
+echo "...done"
+
+echo "Changing to $dir.."
+cd $dir
+
+echo "Moving any existing files in ~ to $olddir.."
+for entry in $dir/*; do
+  fname=$(basename $entry)
+ 
+  if in_list "$ignorefiles" "$fname"; then
+    echo "-> ignored $fname"
+    continue
+  fi
+  if [[ -e ~/.$fname ]]; then
+    echo "-> moving .$fname"
+    mv ~/.$fname $olddir
+  fi
+  echo "-> create symlink for $fname"
+  ln -s $entry ~/.$fname
+done
+echo "...done"
