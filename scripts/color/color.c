f3d01ed449605cf16cf421259d9eebd8e219e150
Kevin Quinn <kquinn@xes-inc.com>
2017-11-16 09:14:52 -0600

    color: basic ansii_code color printing, not user friendly though

--- scripts/color/color.c
+++ scripts/color/color.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "color.h"
+#include <string.h>
 
+
+void print_code(int ansii_num, char* message, int no_newline) {
+    if (ansii_num > 255 || ansii_num < 0) {
+        printf("Can't use numbers outside range 0-255");
+        exit(1);
+    }
+
+    printf("\e[38;05;%dm%s\e[0m", ansii_num, message);
+    if ( no_newline == 0) {
+        printf("\n");
+    }
+
+}
 
 void cprint(char c, int is_bold, char* message) {
     char* color_string = "color";
--- scripts/color/color.h
+++ scripts/color/color.h
@@ -1,6 +1,7 @@
 #ifndef COLOR_H_
 #define COLOR_H_
 
+void print_code(int, char*, int);
 void cprint(char, int, char*);
 
 #endif // COLOR_H_
